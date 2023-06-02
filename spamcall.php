5bf38b162122b334e35ee22b8663bbcebf3718d9
trickexpert <109221625+trickexpert@users.noreply.github.com>
2023-06-02 19:26:50 +0800

    Update spamcall.php

--- spamcall.php
+++ spamcall.php
@@ -2,7 +2,7 @@
 // Limit 3x Telpon Setiap Satu Nomor
 function send($phone){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_URL, "https://mydigiapp.digi.com.my/auth/requestTac");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
@@ -18,7 +18,7 @@ echo "
         Support     : Kunjungi Website Kami
         Website     : https://termux.id
         Youtube     : Omalip TV
-     Penggunaannya  : Bisa Menggunakan [08/62]
+     Penggunaannya  : Bisa Menggunakan [08/60]
 ###########################################################\n";
 echo "Nomor\nInput : ";
 $nomor = trim(fgets(STDIN));
